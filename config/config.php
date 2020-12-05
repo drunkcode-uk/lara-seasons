a312a89eff01052a7e355391639b0e2dca8724eb
Ray Blair <rblair@codeplayground.io>
2020-12-05 23:04:49 +0000

    WIP
    Signed-off-by: Ray Blair <rblair@codeplayground.io>

--- config/config.php
+++ config/config.php
@@ -12,29 +12,29 @@ return [
         'spring' => [
             'active' => false,
             'between' => [
-                '1st March',
-                '31st May',
+                '1st March 2021',
+                '31st May 2021',
             ]
         ],
         'summer' => [
             'active' => false,
             'between' => [
-                '1st June',
-                '31st August',
+                '1st June 2021',
+                '31st August 2021',
             ]
         ],
         'autumn' => [
             'active' => false,
             'between' => [
-                '1st September',
-                '31st November',
+                '1st September 2021',
+                '31st November 2021',
             ]
         ],
         'winter' => [
             'active' => false,
             'between' => [
-                '1st December',
-                '29th February',
+                '1st December 2020',
+                '29th February 2021',
             ]
         ],
     ]
